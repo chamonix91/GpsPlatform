8c7e2ed523240d1f322f2c9f14bd7d41c2b23f5d
DESKTOP-DFLEMQ3\pc <chamseddine.bezzaouia@gmail.com>
2018-07-16 11:30:53 +0100

    update document vehicle

--- src/ApiGps/AdministrationBundle/Document/Vehicle.php
+++ src/ApiGps/AdministrationBundle/Document/Vehicle.php
@@ -28,6 +28,11 @@ class Vehicle
     private $reg_number;
 
     /**
+     * @MongoDB\Field(type="string")
+     */
+    private $type;
+
+    /**
      * @MongoDB\ReferenceOne(targetDocument="Box", mappedBy="vehicle")
      */
     public $box;
@@ -85,5 +90,27 @@ class Vehicle
     public function getBox()
     {
         return $this->box;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return $this
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string $type
+     */
+    public function getType()
+    {
+        return $this->type;
     }
 }
